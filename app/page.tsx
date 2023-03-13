1ccb3de10c2b057c24b92528b1cbb33d88933e85
Nasrul Fahmi <75nasrulfahmi@gmail.com>
2023-03-13 13:43:24 +0700

    addin neseccery

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,8 @@
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import styles from './page.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
-
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <p>Test</p>
+    <main >
+      
     </main>
   )
 }
