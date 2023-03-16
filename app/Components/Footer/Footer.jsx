5daf7a54f6f9d6df3cc0a7fe0a9b908050f52eaf
Nasrul Fahmi <75nasrulfahmi@gmail.com>
2023-03-16 22:11:41 +0700

    fix footer and navbar

--- app/Components/Footer/Footer.jsx
+++ app/Components/Footer/Footer.jsx
@@ -10,10 +10,10 @@ export default function Footer() {
             <div className="flex justify-center items-center flex-col gap-5
             lg:gap-10 lg:mt-7">
                 <div className="flex justify-center items-center gap-4">
-                    <Link href=""><Image alt="Facebook" src={Facebook} className='w-[15%] lg:w-[100%]' /></Link>
-                    <Link href=""><Image alt="Instagram" src={Instagram} className='w-[15%] lg:w-[100%]' /></Link>
-                    <Link href=""><Image alt="Twitter" src={Twitter} className='w-[15%] lg:w-[100%]' /></Link>
-                    <Link href=""><Image alt="Youtube" src={Youtube} className='w-[15%] lg:w-[100%]' /></Link>
+                    <Link href="" className=""><Image alt="Facebook" src={Facebook} className='' /></Link>
+                    <Link href="" className=""><Image alt="Instagram" src={Instagram} className=' ' /></Link>
+                    <Link href="" className=""><Image alt="Twitter" src={Twitter} className=' ' /></Link>
+                    <Link href="" className=""><Image alt="Youtube" src={Youtube} className=' ' /></Link>
                 </div>
                 <h1 className="lg:text-xl"><span className="text-Gray lg:text-xl">Copyright 2023 - </span> Nasrul Fahmi</h1>
             </div>
--- app/Components/navbar/Navbar.jsx
+++ app/Components/navbar/Navbar.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import { useState } from 'react';
 import Link from "next/link";
 export default function Navbar() {
-  const [isActive, setisActive] = useState(true)
+  const [isActive, setisActive] = useState(false)
 
   const toogleActive = () => {
     setisActive(prevValue => !prevValue)
@@ -35,7 +35,7 @@ export default function Navbar() {
 
       </div>
       {isActive ?
-        <div className="sm:hidden bg-Black2 w-[80%] h-screen text-whiteColor absolute right-[-100%] top-0 shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)] shadow-Black2 transition-all duration-300 ease-in-out  ">
+        <div className="sm:hidden bg-Black2 w-[80%] h-screen text-whiteColor absolute right-0 top-0 shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)] shadow-Black2 transition-all duration-300 ease-in-out  ">
           <div className="flex flex-col justify-center items-end gap-20 mt-20 px-9 py-10">
             <h1 className='text-end text-3xl'>Project</h1>
             <h1 className='text-end text-3xl'>Resume</h1>
@@ -44,14 +44,7 @@ export default function Navbar() {
           </div>
         </div>
         :
-        <div className="sm:hidden bg-Black2 w-[80%] h-screen text-whiteColor absolute right-0 top-0 shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)] shadow-Black2 transition-all duration-300 ease-in-out  ">
-          <div className="flex flex-col justify-center items-end gap-20 mt-20 px-9 py-10">
-            <h1 className='text-end text-3xl'>Project</h1>
-            <h1 className='text-end text-3xl'>Resume</h1>
-            <h1 className='text-end text-3xl'>Contact</h1>
-            <div className="w-20 h-20 rounded-full bg-Gray "></div>
-          </div>
-        </div>}
+        ''}
     </div>
   )
 }
